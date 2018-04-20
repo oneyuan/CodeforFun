1d36119ee7bfd416b7e68fa544b0fa8f1010edcd
oneyuan <oneyuan@hotmail.com>
2018-04-20 12:25:20 +0800

    Solve "Max Consecutive Ones"

--- PythonCode/src/code.py
+++ PythonCode/src/code.py
@@ -836,3 +836,44 @@ class Solution(object):
 #                 ans += 1
 #         return ans
         
+        
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        r = 0
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                count += 1
+                if i == len(nums)-1 and r < count:
+                    r = count
+            else:
+                if count > r:
+                    r = count
+                count = 0
+        return r
+    
+    def findMaxConsecutiveOnes0(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if nums == []:
+            return 0
+        
+        count = 0
+        countMax =0
+        for ele in nums:
+            if ele == 1:
+                count += 1
+            else:
+                if count > countMax:
+                    countMax = count
+                count = 0
+        
+        if count > countMax:
+            countMax = count
+        
+        return countMax
