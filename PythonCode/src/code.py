08e30daba3135206834424a5777dc74b64a89ad8
oneyuan <oneyuan@hotmail.com>
2018-04-30 23:35:16 +0800

    Solve "Two Sum"

--- PythonCode/src/code.py
+++ PythonCode/src/code.py
@@ -1116,7 +1116,7 @@ class Solution(object):
                 return i
             
     
-    def twoSum(self, numbers, target):
+    def twoSumTwo(self, numbers, target):
         """
         :type numbers: List[int]
         :type target: int
@@ -1141,7 +1141,7 @@ class Solution(object):
                     if numbers[i] + numbers[j] == target:
                         return [i+1,j+1]
                     
-    def twoSum1(self, numbers, target):
+    def twoSumTwo1(self, numbers, target):
         """
         :type numbers: List[int]
         :type target: int
@@ -1163,7 +1163,7 @@ class Solution(object):
         else:
             return []
         
-    def twoSum0(self, numbers, target):
+    def twoSumTwo0(self, numbers, target):
       
         """
         :type numbers: List[int]
@@ -1506,4 +1506,37 @@ class Solution(object):
         return len(num)
     
     
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        k = []
+        dic = {}
+        for i in range(len(nums)):
+            if nums[i] in dic:
+                if nums[i] * 2 == target:
+                    return [dic[nums[i]],i]
+            else:
+                dic[nums[i]] = i
+            k.append(target - nums[i])
+        for j in range(len(k)):
+            if k[j] in dic:
+                if k[j] != nums[j]:
+                    return [j,dic[k[j]]]
     
+    def twoSum0(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        tmp = {}
+        for i in range(len(nums)):
+            if target - nums[i] in tmp:
+                return(tmp[target - nums[i]], i)
+            else:
+                tmp[nums[i]] = i;
+        
+        
--- PythonCode/src/main.py
+++ PythonCode/src/main.py
@@ -5,7 +5,7 @@ w = code.Solution()
 a = [-1,-2,-3]
 matrix = [[0,19,54,9],[47,9,19,54],[11,47,9,19]]
 cost = [10, 15, 20]
-nums = [4,4,0,1,0,2]
+nums = [2, 7, 11, 15]
 val = 0
 k = 4
 nums1 = [4,2,1]
@@ -14,7 +14,7 @@ M = [[2,3,4],[5,6,7],[8,9,10],[11,12,13],[14,15,16]]
 flowerbed = [0,1,0]
 n = 1
 numbers = [1,2,3,4,4,9,56,90]
-target = 8
+target = 9
 #print(w.maximumProduct2(a))
 #print(w.isToeplitzMatrix(matrix))
 #print(len(matrix[1]))
@@ -32,4 +32,5 @@ target = 8
 #print(w.containsNearbyDuplicate(nums, k))
 #print(w.rotate(nums, k))
 #print(w.twoSum(numbers, target))
-print(w.removeElement(nums, val))
+#print(w.removeElement(nums, val))
+print(w.twoSum(nums, target))
