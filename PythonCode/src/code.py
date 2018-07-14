888246c4a3b0a7ab05c4d7563de19ba90236a2d5
oneyuan <oneyuan@hotmail.com>
2018-07-14 14:26:31 +0800

    Solve "6. ZigZag Conversion"

--- PythonCode/src/code.py
+++ PythonCode/src/code.py
@@ -3224,4 +3224,47 @@ class Solution(object):
             return 0
         
     
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows < len(s):
+            res = [s[i] for i in range(numRows)]
+        else:
+            return s
+        if numRows == 1:
+            return s
+        for i in range(numRows, len(s)):
+            c = 2*(numRows - 1)
+            t = i % c
+            if t in range(numRows):
+                res[t] += s[i]
+            else:
+                res[c-t] += s[i]
+        return "".join(res)
+
+    def convert0(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1: return s
+        temp = ['' for i in range(numRows)]
+        index = -1
+        step = 1
+        for i in range(len(s)):
+            index += step
+            if index == numRows:
+                index -= 2
+                step = -1
+            if index == -1:
+                index = 1
+                step = 1
+            temp[index] += s[i]
+        return "".join(temp)
+            
+    
     
