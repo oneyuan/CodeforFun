e594e8952351d5d26c45320f76906998c7c769a5
oneyuan <oneyuan@hotmail.com>
2018-07-14 14:48:38 +0800

    Solve "9. Palindrome Number"

--- PythonCode/src/code.py
+++ PythonCode/src/code.py
@@ -3347,4 +3347,26 @@ class Solution(object):
             return 0
         
     
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        s = str(x)
+        l = [s[i] for i in range(len(s))]
+        l.reverse()
+        t = "".join(l)
+        if s == t:
+            return True
+        else:
+            return False
+        
+    def isPalindrome0(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return str(x) == str(x)[::-1]
+    
+    
     
