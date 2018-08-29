0ddd101ba789e361f0b2524580a97a0221a346af
oneyuan <oneyuan@hotmail.com>
2018-08-29 15:44:16 +0800

    Solve "33. Search in Rotated Sorted Array"

--- PythonCode/src/code.py
+++ PythonCode/src/code.py
@@ -4472,4 +4472,46 @@ class Solution(object):
         return max_res
     
     
-            
+    def search_1(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        n = len(nums)
+        low = 0 
+        high = n - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if target == nums[mid]:
+                return mid
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
+    
+    def search0(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        '''
+        if target in nums:
+            return nums.index(target)
+        else:return -1
+        '''
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return i
+        return -1
+    
+    
+    
