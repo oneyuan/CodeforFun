3750f5a79bfce5b431f334f52b97e465d79e5650
oneyuan <oneyuan@hotmail.com>
2019-03-27 23:32:38 +0800

    minor change

--- PythonCode/src/572.subtree-of-another-tree.py
+++ PythonCode/src/572.subtree-of-another-tree.py
@@ -11,37 +11,37 @@
 # Total Submissions: 222.2K
 # Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
 #
-# 
+#
 # Given two non-empty binary trees s and t, check whether tree t has exactly
 # the same structure and node values with a subtree of s. A subtree of s is a
 # tree consists of a node in s and all of this node's descendants. The tree s
 # could also be considered as a subtree of itself.
-# 
-# 
+#
+#
 # Example 1:
-# 
+#
 # Given tree s:
-# 
+#
 # ⁠    3
 # ⁠   / \
 # ⁠  4   5
 # ⁠ / \
 # ⁠1   2
-# 
+#
 # Given tree t:
-# 
-# ⁠  4 
+#
+# ⁠  4
 # ⁠ / \
 # ⁠1   2
-# 
+#
 # Return true, because t has the same structure and node values with a subtree
 # of s.
-# 
-# 
+#
+#
 # Example 2:
-# 
+#
 # Given tree s:
-# 
+#
 # ⁠    3
 # ⁠   / \
 # ⁠  4   5
@@ -49,15 +49,15 @@
 # ⁠1   2
 # ⁠   /
 # ⁠  0
-# 
+#
 # Given tree t:
-# 
+#
 # ⁠  4
 # ⁠ / \
 # ⁠1   2
-# 
+#
 # Return false.
-# 
+#
 #
 # Definition for a binary tree node.
 # class TreeNode:
@@ -65,6 +65,7 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
+
 
 class Solution:
     def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
@@ -95,21 +96,21 @@ class Solution:
         return left or right
 
     def isSubtree0(self, s, t):
-     	def compare(s, t, checkEqual):
-			if not s and not t:
-				return True
-			if not s or not t:
-				return False
+        def compare(s, t, checkEqual):
+            if not s and not t:
+                return True
+            if not s or not t:
+                return False
 
-			if s.val != t.val:
-				if checkEqual:
-					return False
-				else:
-					return compare(s.left, t, False) or compare(s.right, t, False)
-			else:
-				return (compare(s.left, t.left, True) and compare(s.right, t.right, True)) or \
+            if s.val != t.val:
+                if checkEqual:
+                    return False
+                else:
+                    return compare(s.left, t, False) or compare(s.right, t, False)
+            else:
+                return (compare(s.left, t.left, True) and compare(s.right, t.right, True)) or \
                                     compare(s.left, t, False) or compare(
                                         s.right, t, False)
 
-		return compare(s, t, False)
+        return compare(s, t, False)
 
