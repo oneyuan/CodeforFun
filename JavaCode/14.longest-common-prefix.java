9f8da5112e974db0f5e6d8d5babd63f35b4dca39
oneyuan <oneyuan@hotmail.com>
2021-03-31 23:05:39 +0800

    Solve "14. Longest Common Prefix"

---
+++ JavaCode/14.longest-common-prefix.java
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode id=14 lang=java
+ *
+ * [14] Longest Common Prefix
+ *
+ * https://leetcode.com/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (36.15%)
+ * Likes:    3863
+ * Dislikes: 2169
+ * Total Accepted:    976.4K
+ * Total Submissions: 2.7M
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * Write a function to find the longest common prefix string amongst an array
+ * of strings.
+ * 
+ * If there is no common prefix, return an empty string "".
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= strs.length <= 200
+ * 0 <= strs[i].length <= 200
+ * strs[i] consists of only lower-case English letters.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0){
+            return "";
+        }
+        String prefix = strs[0];
+        for(int i = 1; i < strs.length; i ++){
+            while(strs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length()-1);
+                if(prefix.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+}
+// @lc code=end
+
