b8fca8c8240fb6140208c1c40f6192cae6ca5db9
oneyuan <oneyuan@hotmail.com>
2021-03-31 23:42:00 +0800

    Solve ”21. Merge Two Sorted Lists“

---
+++ JavaCode/21.merge-two-sorted-lists.java
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode id=21 lang=java
+ *
+ * [21] Merge Two Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ * algorithms
+ * Easy (56.20%)
+ * Likes:    6425
+ * Dislikes: 751
+ * Total Accepted:    1.4M
+ * Total Submissions: 2.4M
+ * Testcase Example:  '[1,2,4]\n[1,3,4]'
+ *
+ * Merge two sorted linked lists and return it as a sorted list. The list
+ * should be made by splicing together the nodes of the first two lists.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: l1 = [1,2,4], l2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: l1 = [], l2 = []
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: l1 = [], l2 = [0]
+ * Output: [0]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in both lists is in the range [0, 50].
+ * -100 <= Node.val <= 100
+ * Both l1 and l2 are sorted in non-decreasing order.
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode tmp = new ListNode(0);
+        ListNode res = tmp;
+        while(l1 != null && l2 != null){
+            int val1 = l1.val;
+            int val2 = l2.val;
+            if(val1 < val2){
+                tmp.next = new ListNode(val1);
+                l1 = l1.next;
+            }else{
+                tmp.next = new ListNode(val2);
+                l2 = l2.next;
+            }
+            tmp = tmp.next;
+        }
+        if(l1 != null){
+            tmp.next = l1;
+        }
+        if(l2 != null){
+            tmp.next = l2;
+        }
+        return res.next;
+    }
+}
+// @lc code=end
+
