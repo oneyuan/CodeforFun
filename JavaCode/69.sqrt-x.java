d0458162e33b00b2980b645319fb8c7bf65cc65d
oneyuan <oneyuan@hotmail.com>
2021-04-22 23:45:51 +0800

    Solve "69. Sqrt(x)"

---
+++ JavaCode/69.sqrt-x.java
@@ -0,0 +1,57 @@
+/*
+ * @lc app=leetcode id=69 lang=java
+ *
+ * [69] Sqrt(x)
+ *
+ * https://leetcode.com/problems/sqrtx/description/
+ *
+ * algorithms
+ * Easy (35.17%)
+ * Likes:    1941
+ * Dislikes: 2335
+ * Total Accepted:    713.5K
+ * Total Submissions: 2M
+ * Testcase Example:  '4'
+ *
+ * Given a non-negative integer x, compute and return the square root of x.
+ * 
+ * Since the return type is an integer, the decimal digits are truncated, and
+ * only the integer part of the result is returned.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: x = 4
+ * Output: 2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: x = 8
+ * Output: 2
+ * Explanation: The square root of 8 is 2.82842..., and since the decimal part
+ * is truncated, 2 is returned.
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= x <= 2^31 - 1
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int mySqrt(int x) {
+        long r = x;
+        while(r*r > x){
+            r = (r + x/r)/2;
+        }
+        return (int) r;
+    }
+}
+// @lc code=end
+
