679222b7fa096678e2987a8c0e2c7946cbfe9f8d
oneyuan <oneyuan@hotmail.com>
2021-06-03 23:04:11 +0800

    Solve “122. Best Time to Buy and Sell Stock II”

---
+++ JavaCode/122.best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,83 @@
+/*
+ * @lc app=leetcode id=122 lang=java
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
+ *
+ * algorithms
+ * Easy (59.11%)
+ * Likes:    4319
+ * Dislikes: 2040
+ * Total Accepted:    844.3K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[7,1,5,3,6,4]'
+ *
+ * You are given an array prices where prices[i] is the price of a given stock
+ * on the i^th day.
+ * 
+ * Find the maximum profit you can achieve. You may complete as many
+ * transactions as you like (i.e., buy one and sell one share of the stock
+ * multiple times).
+ * 
+ * Note: You may not engage in multiple transactions simultaneously (i.e., you
+ * must sell the stock before you buy again).
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 7
+ * Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit
+ * = 5-1 = 4.
+ * Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 =
+ * 3.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: prices = [1,2,3,4,5]
+ * Output: 4
+ * Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit
+ * = 5-1 = 4.
+ * Note that you cannot buy on day 1, buy on day 2 and sell them later, as you
+ * are engaging multiple transactions at the same time. You must sell before
+ * buying again.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: prices = [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e., max profit = 0.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= prices.length <= 3 * 10^4
+ * 0 <= prices[i] <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        int res = 0;
+        int n = prices.length;
+        for(int i=0; i < n-1; i++){
+            int tmp = prices[i+1] - prices[i];
+            if(tmp > 0){
+                res += tmp;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
