c80e7e876d9d1371a5ae685cfa4355342c86497d
oneyuan <oneyuan@hotmail.com>
2021-06-03 22:59:31 +0800

    Solve “121. Best Time to Buy and Sell Stock”

---
+++ JavaCode/121.best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=121 lang=java
+ *
+ * [121] Best Time to Buy and Sell Stock
+ *
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+ *
+ * algorithms
+ * Easy (51.99%)
+ * Likes:    8797
+ * Dislikes: 370
+ * Total Accepted:    1.3M
+ * Total Submissions: 2.6M
+ * Testcase Example:  '[7,1,5,3,6,4]'
+ *
+ * You are given an array prices where prices[i] is the price of a given stock
+ * on the i^th day.
+ * 
+ * You want to maximize your profit by choosing a single day to buy one stock
+ * and choosing a different day in the future to sell that stock.
+ * 
+ * Return the maximum profit you can achieve from this transaction. If you
+ * cannot achieve any profit, return 0.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: prices = [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit
+ * = 6-1 = 5.
+ * Note that buying on day 2 and selling on day 1 is not allowed because you
+ * must buy before you sell.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: prices = [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transactions are done and the max profit =
+ * 0.
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= prices.length <= 10^5
+ * 0 <= prices[i] <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length == 1){
+            return 0;
+        }
+        int n = prices.length;
+        int start = prices[0];
+        int res = 0;
+        for(int i = 1; i < n; i++){
+            if(prices[i]-start < 0){
+                start = prices[i];
+            }else{
+                res = Math.max(res, prices[i]-start);
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
