7571e91d02eb8ab440a928a49be40df0514e361d
oneyuan <oneyuan@hotmail.com>
2021-07-15 22:42:13 +0800

    Solve "15. 3Sum"

---
+++ JavaCode/15.3-sum.java
@@ -0,0 +1,77 @@
+/*
+ * @lc app=leetcode id=15 lang=java
+ *
+ * [15] 3Sum
+ *
+ * https://leetcode.com/problems/3sum/description/
+ *
+ * algorithms
+ * Medium (28.69%)
+ * Likes:    11719
+ * Dislikes: 1158
+ * Total Accepted:    1.4M
+ * Total Submissions: 4.8M
+ * Testcase Example:  '[-1,0,1,2,-1,-4]'
+ *
+ * Given an integer array nums, return all the triplets [nums[i], nums[j],
+ * nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] +
+ * nums[k] == 0.
+ * 
+ * Notice that the solution set must not contain duplicate triplets.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [-1,0,1,2,-1,-4]
+ * Output: [[-1,-1,2],[-1,0,1]]
+ * Example 2:
+ * Input: nums = []
+ * Output: []
+ * Example 3:
+ * Input: nums = [0]
+ * Output: []
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= nums.length <= 3000
+ * -10^5 <= nums[i] <= 10^5
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if(nums.length < 3){
+            return res;
+        }
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length-2; i++){
+            if(i == 0 || (i > 0 && nums[i] != nums[i-1])){
+                int left = i+1, right = nums.length-1, sum = 0-nums[i];
+                while(left < right){
+                    if(nums[left] + nums[right] == sum){
+                        res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                        while(left < right && nums[left] == nums[left+1]){
+                            left++;
+                        }
+                        while(left < right && nums[right] == nums[right-1]){
+                            right--;
+                        }
+                        left++;
+                        right--;
+                    }else if(nums[left] + nums[right] < sum){
+                        left++;
+                    }else{
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
+
