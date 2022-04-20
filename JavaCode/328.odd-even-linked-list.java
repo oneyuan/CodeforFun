1b2e5323276d17fe8006d42e7adea8918680e58d
oneyuan <oneyuan@hotmail.com>
2022-04-20 22:53:26 +0800

    Solve "328. Odd Even Linked List"

---
+++ JavaCode/328.odd-even-linked-list.java
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=328 lang=java
+ *
+ * [328] Odd Even Linked List
+ *
+ * https://leetcode.com/problems/odd-even-linked-list/description/
+ *
+ * algorithms
+ * Medium (57.62%)
+ * Likes:    5201
+ * Dislikes: 383
+ * Total Accepted:    519.7K
+ * Total Submissions: 873.5K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given the head of a singly linked list, group all the nodes with odd indices
+ * together followed by the nodes with even indices, and return the reordered
+ * list.
+ * 
+ * The first node is considered odd, and the second node is even, and so on.
+ * 
+ * Note that the relative order inside both the even and odd groups should
+ * remain as it was in the input.
+ * 
+ * You must solve the problem in O(1) extra space complexity and O(n) time
+ * complexity.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,3,4,5]
+ * Output: [1,3,5,2,4]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [2,1,3,5,6,4,7]
+ * Output: [2,3,6,7,1,5,4]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * n == number of nodes in the linked list
+ * 0 <= n <= 10^4
+ * -10^6 <= Node.val <= 10^6
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(null == head){
+            return head;
+        }
+        ListNode odd = head, even = head.next, evenHead = even;
+        while(null != even && null != even.next){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
+// @lc code=end
+
