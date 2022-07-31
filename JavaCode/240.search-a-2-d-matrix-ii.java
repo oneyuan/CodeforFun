9df7250ff0f8e91046212ebfaf0f898a02c61961
oneyuan <oneyuan@hotmail.com>
2022-07-31 22:53:55 +0800

    solve "240. Search a 2D Matrix II"

---
+++ JavaCode/240.search-a-2-d-matrix-ii.java
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode id=240 lang=java
+ *
+ * [240] Search a 2D Matrix II
+ *
+ * https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+ *
+ * algorithms
+ * Medium (45.86%)
+ * Likes:    8851
+ * Dislikes: 147
+ * Total Accepted:    689.6K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]\n5'
+ *
+ * Write an efficient algorithm that searches for a value target in an m x n
+ * integer matrix matrix. This matrix has the following properties:
+ * 
+ * 
+ * Integers in each row are sorted in ascending from left to right.
+ * Integers in each column are sorted in ascending from top to bottom.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: matrix =
+ * [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+ * target = 5
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: matrix =
+ * [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]],
+ * target = 20
+ * Output: false
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= n, m <= 300
+ * -10^9 <= matrix[i][j] <= 10^9
+ * All the integers in each row are sorted in ascending order.
+ * All the integers in each column are sorted in ascending order.
+ * -10^9 <= target <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        if(m <= 0){
+            return false;
+        }
+        int n = matrix[0].length;
+        int x = m-1;
+        int y = 0;
+        while(x >= 0 && y < n){
+            if(matrix[x][y] == target){
+                return true;
+            }
+            if(matrix[x][y] > target){
+                x--;
+            }else{
+                y++;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
+
