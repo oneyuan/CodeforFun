6032e59045b597c0bb5a2c55d243c26db684e94e
oneyuan <oneyuan@hotmail.com>
2022-08-12 23:22:32 +0800

    Solve "230. Kth Smallest Element in a BST"

---
+++ JavaCode/230.kth-smallest-element-in-a-bst.java
@@ -0,0 +1,110 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=230 lang=java
+ *
+ * [230] Kth Smallest Element in a BST
+ *
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (63.66%)
+ * Likes:    7938
+ * Dislikes: 137
+ * Total Accepted:    890.8K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * Given the root of a binary search tree, and an integer k, return the k^th
+ * smallest value (1-indexed) of all the values of the nodes in the tree.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,1,4,null,2], k = 1
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [5,3,6,2,4,null,null,1], k = 3
+ * Output: 3
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the tree is n.
+ * 1 <= k <= n <= 10^4
+ * 0 <= Node.val <= 10^4
+ * 
+ * 
+ * 
+ * Follow up: If the BST is modified often (i.e., we can do insert and delete
+ * operations) and you need to find the kth smallest frequently, how would you
+ * optimize?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list = inorderTraverse(root, new ArrayList<Integer>());
+        return list.get(k-1);
+    }
+
+    public List<Integer> inorderTraverse(TreeNode root, ArrayList<Integer> list){
+        if(root == null){
+            return list;
+        }
+        inorderTraverse(root.left, list);
+        list.add(root.val);
+        inorderTraverse(root.right, list);
+        return list;
+    }
+
+    public int kthSmallest0(TreeNode root, int k){
+        if(root == null){
+            return -1;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+        TreeNode cur = root;
+        int count = 0;
+        while(cur != null || !stack.isEmpty()){
+            while(cur != null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            count++;
+            if(count == k){
+                return cur.val;
+            }
+            cur = cur.right;
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
